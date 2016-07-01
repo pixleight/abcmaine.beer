f6a15b556e435a84c291f393cfea3394d85869e2
Chris Violette <cvioletterun@gmail.com>
2016-07-01 08:55:47 -0400

    Adding favicons

--- templates/head.php
+++ templates/head.php
@@ -1,6 +1,14 @@
+<?php use Roots\Sage\Assets; ?>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="<?= Assets\asset_path('images/favicon.png'); ?>">
+  <!--[if IE]>
+    <link rel="shortcut icon" href="<?= Assets\asset_path('images/favicon.ico'); ?>">
+  <![endif]-->
+  <meta name="theme-color" content="#FDC625">
+  <meta name="msapplication-navbutton-color" content="#FDC625">
+  <meta name="apple-mobile-web-app-status-bar-style" content="#FDC625">
   <?php wp_head(); ?>
 </head>
